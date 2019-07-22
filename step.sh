31d1853fff6d1c44c43edd04099041145a0d4acf
Edward Jaszke <edward.jaszke@imagene.me>
2019-07-22 21:11:51 +0200

    Added bitrise step

---
+++ step.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+echo "Ensure Node 6 LTS"
+
+brew uninstall node
+brew tap homebrew/versions
+brew install homebrew/versions/node10
+node -v
+
+VERSION=""
+
+echo "Available NativeScript versions: $(npm view nativescript versions)"
+
+if [ ! -z "${version}" ] ; then
+    VERSION="@${version}"
+fi
+
+echo "n
+y
+y
+q
+agree
+N
+n
+n" | npm install -g ${npm_options} nativescript${VERSION}
+echo n | tns doctor
